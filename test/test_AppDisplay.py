68076237772dd1dafc03cbfb10e5a279a92ba2b3
matthandi <matthandi@online.de>
2020-08-22 15:00:06 +0200

    automatically main directory file deployment (update of test/test_AppDisplay.py)

--- test/test_AppDisplay.py
+++ test/test_AppDisplay.py
@@ -18,18 +18,18 @@ def test_appdisplay():
     ab = AppDisplay.CAppDisplay(app_device)
     assert ab.device == app_device
     assert ab.bdevice == b"display"
-    assert ab.client_id == "contXdisplay"
+    assert ab.client_id == "contXdisplay0"
     assert ab.topic == b'contX'
     assert ab.github_repo == "https://github.com/matthandi/mrscontxUpdater"
     assert ab.main_dir == "main"
     assert ab.module == ""
     assert ab.user_agent == {'User-Agent':'contX-app'}
-    assert ab.subscribe_cmnd_version_msg == b'contX/display/cmnd/version'
-    assert ab.topic_cmnd_settext_msg     == b'contX/display/cmnd/settext'
-    assert ab.topic_cmnd_fill_msg        == b'contX/display/cmnd/fill'
-    assert ab.topic_cmnd_gettext_msg     == b'contX/display/cmnd/gettext'
+    assert ab.subscribe_cmnd_version_msg == b'contX/display/0/cmnd/version'
+    assert ab.topic_cmnd_settext_msg     == b'contX/display/0/cmnd/settext'
+    assert ab.topic_cmnd_fill_msg        == b'contX/display/0/cmnd/fill'
+    assert ab.topic_cmnd_gettext_msg     == b'contX/display/0/cmnd/gettext'
     # replies
-    assert ab.topic_text_msg             == b'contX/display/text'
+    assert ab.topic_text_msg             == b'contX/display/0/text'
 
     assert ab.dspl_onoff_state           == 1
 
@@ -62,15 +62,15 @@ def test_display_subscribe_cb(mock_machine,mock_umqtt,mock_network,mock_ssd1306)
     # test request display fill
     mock_umqtt.reset_mock()
     mock_machine.reset_mock()
-    ab.mqtt_display_subscribe_cb(b"contX/display/cmnd/fill",b'0')
+    ab.mqtt_display_subscribe_cb(b"contX/display/1/cmnd/fill",b'0')
     mock_ssd1306.return_value.fill.assert_called_with(0)
-    ab.mqtt_display_subscribe_cb(b"contX/display/cmnd/fill",b'1')
+    ab.mqtt_display_subscribe_cb(b"contX/display/1/cmnd/fill",b'1')
     mock_ssd1306.return_value.fill.assert_called_with(1)
 
     # test request set text
     mock_umqtt.reset_mock()
     mock_machine.reset_mock()
-    ab.mqtt_display_subscribe_cb(b"contX/display/cmnd/settext",b"hello world,0,5")
+    ab.mqtt_display_subscribe_cb(b"contX/display/1/cmnd/settext",b"hello world,0,5")
     mock_ssd1306.return_value.text.assert_called_with('hello world',0,5)
 
 @patch("AppDisplay.umqtt.simple.MQTTClient.subscribe")
@@ -84,14 +84,14 @@ def test_begin(mock_machine,mock_network,mock_umqtt):
     ab.begin()
     # checking subscribes
     subscribe_calls = [
-                        call(b'contX/display/cmnd/version'),
-                        call(b'contX/display/cmnd/repoversion'),
-                        call(b'contX/display/cmnd/download'),
-                        call(b'contX/display/cmnd/install'),
-                        call(b'contX/display/cmnd/memfree'),
-                        call(b'contX/display/cmnd/settext'),
-                        call(b'contX/display/cmnd/gettext'),
-                        call(b'contX/display/cmnd/fill')
+                        call(b'contX/display/1/cmnd/version'),
+                        call(b'contX/display/1/cmnd/repoversion'),
+                        call(b'contX/display/1/cmnd/download'),
+                        call(b'contX/display/1/cmnd/install'),
+                        call(b'contX/display/1/cmnd/memfree'),
+                        call(b'contX/display/1/cmnd/settext'),
+                        call(b'contX/display/1/cmnd/gettext'),
+                        call(b'contX/display/1/cmnd/fill')
                       ]
     mock_umqtt.assert_has_calls(subscribe_calls)
 
