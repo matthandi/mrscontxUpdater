2928869dc0cca7a34e8d96762efeb2e75f7da7dd
matthandi <matthandi@online.de>
2020-08-29 23:22:10 +0200

    automatically main directory file deployment (update of main/AppPwm.py)

--- main/AppPwm.py
+++ main/AppPwm.py
@@ -20,20 +20,23 @@ class CAppPwm(AppBase.CAppBase):
         constructor
         """
         super().__init__(device=device,device_id = device_id,github_repo=github_repo)
-        self.position = 0
-        self.freqency = 50
-        self.pwm_pin = pwm_pin
-        self.pwm = machine.PWM(pwm_pin,freq=self.freqency,duty=self.position)
+        self.duty     = 77
+        self.frequency = 50
+        self.pwm_pin  = machine.Pin(pwm_pin,machine.Pin.OUT)
+        self.pwm = machine.PWM(self.pwm_pin,freq=self.frequency,duty=self.duty)
         self.set_pwm_devicename_id()
 
     def set_pwm_devicename_id(self):
         self.prefix_msg = self.topic + b"/" + self.bdevice + b"/" + self.bdevice_id
         # command messages
-        self.topic_cmnd_set_pos_msg = self.prefix_msg + b"/cmnd/setpos"
-        self.topic_cmnd_get_pos_msg = self.prefix_msg + b"/cmnd/getpos"
+        self.topic_cmnd_set_duty_msg = self.prefix_msg + b"/cmnd/setduty"
+        self.topic_cmnd_get_duty_msg = self.prefix_msg + b"/cmnd/getduty"
+        self.topic_cmnd_set_frequency_msg = self.prefix_msg + b"/cmnd/setfrequency"
+        self.topic_cmnd_get_frequency_msg = self.prefix_msg + b"/cmnd/getfrequency"
         # publish messages
-        #   publish pos
-        self.topic_pos_msg = self.prefix_msg + b"/pos"
+        #   publish duty
+        self.topic_duty_msg      = self.prefix_msg + b"/duty"
+        self.topic_frequency_msg = self.prefix_msg + b"/frequency"
 
     def mqtt_pwm_subscribe_cb(self,topic,payload):
         """
@@ -42,15 +45,23 @@ class CAppPwm(AppBase.CAppBase):
         # call callback function from base class
         super().mqtt_subscribe_cb(topic,payload)
 
-        # request set pos command
-        if topic == self.topic_cmnd_set_pos_msg:
-            self.position = int(str(payload).encode('utf-8')) * 1024/100
-            self.pwm.duty(self.position)
+        # request set duty command
+        if topic == self.topic_cmnd_set_duty_msg:
+            self.duty = int(payload)
+            self.pwm.duty(self.duty)
 
-        # request get pos command
-        if topic == self.topic_cmnd_get_pos_msg:
-            self.mqtt_client.publish(self.topic_pos_msg,str(self.position))
+        # request get duty command
+        if topic == self.topic_cmnd_get_duty_msg:
+            self.mqtt_client.publish(self.topic_duty_msg,str(self.duty).encode('utf-8'))
 
+        # request set frequency command
+        if topic == self.topic_cmnd_set_frequency_msg:
+            self.frequency = int(payload)
+            self.pwm.freq(self.frequency)
+
+        # request get frequency command
+        if topic == self.topic_cmnd_get_frequency_msg:
+            self.mqtt_client.publish(self.topic_frequency_msg,str(self.frequency).encode('utf-8'))
 
     def begin(self):
         """
@@ -58,15 +69,16 @@ class CAppPwm(AppBase.CAppBase):
         """
         super().begin()
         self.set_pwm_devicename_id()
-        # TODO: add your init calls here
-        #       ...
-
-        # TODO: add your subscribes to mqtt messages here
-        #       ...
-        # subscribe to command message for set pos
-        self.mqtt_subscribe_to_msg(self.topic_cmnd_set_pos_msg)
-        # subscribe to command message for get pos
-        self.mqtt_subscribe_to_msg(self.topic_cmnd_get_pos_msg)
+        # subscribe to command message for set duty
+        self.mqtt_subscribe_to_msg(self.topic_cmnd_set_duty_msg)
+        # subscribe to command message for get duty
+        self.mqtt_subscribe_to_msg(self.topic_cmnd_get_duty_msg)
+        # subscribe to command message for set frequency
+        self.mqtt_subscribe_to_msg(self.topic_cmnd_set_frequency_msg)
+        # subscribe to command message for get frequency
+        self.mqtt_subscribe_to_msg(self.topic_cmnd_get_frequency_msg)
+        # overwrite callback of base class
+        self.set_subscribe_cb(self.mqtt_pwm_subscribe_cb)
 
 def main():   
     """
