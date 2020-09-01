f87d7d5412cb92c74986149ef170805ddc288e3d
matthandi <matthandi@online.de>
2020-09-01 15:54:43 +0200

    automatically main directory file deployment (update of test/test_AppDisplay.py)

--- test/test_AppDisplay.py
+++ test/test_AppDisplay.py
@@ -98,6 +98,7 @@ def test_begin(mock_machine,mock_network,mock_umqtt):
                         call(b'contX/display/1/cmnd/download'),
                         call(b'contX/display/1/cmnd/install'),
                         call(b'contX/display/1/cmnd/memfree'),
+                        call(b'contX/display/1/cmnd/memalloc'),
                         call(b'contX/display/1/cmnd/reboot'),
                         call(b'contX/display/1/cmnd/getip'),
                         call(b'contX/display/1/cmnd/settext'),
