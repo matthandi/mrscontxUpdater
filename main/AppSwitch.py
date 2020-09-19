fd9021890b718e43be238159a74a5e53ddb936d8
matthandi <matthandi@online.de>
2020-09-19 22:27:39 +0200

    automatically main directory file deployment (update of main/AppSwitch.py)

--- main/AppSwitch.py
+++ main/AppSwitch.py
@@ -11,31 +11,39 @@ class CAppSwitch(AppBase.CAppBase):
     inherited class for a switch application
     """
 
-    def __init__(self, device='switch',device_id="0",btn_pin = AppBase.CAppBase.GPIO26,github_repo="https://github.com/matthandi/mrscontxUpdater"):
+    def __init__(self, device='switch',device_id="0",btn_pin = AppBase.CAppBase.GPIO26,btn1_pin = AppBase.CAppBase.GPIO4,github_repo="https://github.com/matthandi/mrscontxUpdater"):
         """
         constructor
         """
         super().__init__(device=device,device_id=device_id,github_repo=github_repo)
-        self.btn_pin = btn_pin
-        self.last_state = 0
+        self.btn_pin    = btn_pin
+        self.btn1_pin   = btn1_pin
+        self.last_state  = 0
+        self.last_state1 = 0
         self.set_switch_devicename_id()
 
     def set_switch_devicename_id(self):
         # command messages
         #   request for state
-        self.topic_cmnd_state_msg = self.topic + b"/" + self.bdevice + b"/" + self.bdevice_id + b"/cmnd/state"
+        self.topic_cmnd_state_msg  = self.topic + b"/" + self.bdevice + b"/" + self.bdevice_id + b"/cmnd/state"
+        self.topic_cmnd_state1_msg = self.topic + b"/" + self.bdevice + b"/" + self.bdevice_id + b"/cmnd/state1"
         # publish messages
         #   publish state of switch
-        self.topic_state_msg = self.topic + b"/" + self.bdevice + b"/" + self.bdevice_id + b"/state"
+        self.topic_state_msg  = self.topic + b"/" + self.bdevice + b"/" + self.bdevice_id + b"/state"
+        self.topic_state1_msg = self.topic + b"/" + self.bdevice + b"/" + self.bdevice_id + b"/state1"
 
     def create_switch(self):
         """
         create a button interface
         """
-        self.btn = machine.Pin(self.btn_pin, machine.Pin.IN)
+        self.btn  = machine.Pin(self.btn_pin,  machine.Pin.IN)
+        self.btn1 = machine.Pin(self.btn1_pin, machine.Pin.IN)
 
     def publish_switch_state(self):
         self.mqtt_client.publish(self.topic_state_msg,bytes(str(self.last_state),"utf-8"))
+
+    def publish_switch_state1(self):
+        self.mqtt_client.publish(self.topic_state1_msg,bytes(str(self.last_state1),"utf-8"))
 
     def check_switch(self):
         """
@@ -47,6 +55,16 @@ class CAppSwitch(AppBase.CAppBase):
             self.last_state = act_state
             self.publish_switch_state()
 
+    def check_switch1(self):
+        """
+        checks the button state1 - and sends message on change
+        """
+        act_state1 = self.btn1.value()
+        # state changed
+        if self.last_state1 != act_state1:
+            self.last_state1 = act_state1
+            self.publish_switch_state1()
+
     def mqtt_switch_subscribe_cb(self,topic,payload):
         """
         callback function for mqtt subscribes
@@ -55,6 +73,8 @@ class CAppSwitch(AppBase.CAppBase):
         super().mqtt_subscribe_cb(topic,payload)
         if topic == self.topic_cmnd_state_msg:
             self.publish_switch_state()
+        if topic == self.topic_cmnd_state1_msg:
+            self.publish_switch_state1()
 
     def begin(self):
         """
@@ -66,6 +86,8 @@ class CAppSwitch(AppBase.CAppBase):
         self.create_switch()
         # subscribe to command message for state
         self.mqtt_subscribe_to_msg(self.topic_cmnd_state_msg)
+        # subscribe to command message for state1
+        self.mqtt_subscribe_to_msg(self.topic_cmnd_state1_msg)
         # overwrite callback of base class
         self.set_subscribe_cb(self.mqtt_switch_subscribe_cb)
 
@@ -77,6 +99,7 @@ def main():
     app_switch.begin()
     while True:
         app_switch.check_switch()
+        app_switch.check_switch1()
         app_switch.check_msg()
         time.sleep(0.2)
 
